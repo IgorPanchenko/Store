839086be8e16eca84c680a77b77056293f1efcf2
IgorPanchenko <goldman160594@gmail.com>
2023-04-01 18:01:23 +0300

    add models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_image')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import ProductCategory, Product
+
+
+def index(request):
+    return render(request, 'products/index.html')
+
+def products(request):
+    context = {
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context)
